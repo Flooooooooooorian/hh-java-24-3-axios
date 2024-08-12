194fe05b56d1ee3c3c2880e4120faec554a80aee
Flooooooooooorian <florian.weber@neuefische.de>
2024-08-12 14:52:11 +0200

    Add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
 import './App.css'
 import CharacterGallery from "./components/CharacterGallery.tsx";
-import {useState} from "react";
-import {response} from "./Response.ts";
+import {useEffect, useState} from "react";
 import {Route, Routes} from "react-router-dom";
 import NewCharacter from "./components/NewCharacter.tsx";
 import {RickAndMortyCharacter} from "./types/RickAndMortyCharacter.ts";
+import axios from "axios";
 
 export default function App() {
-    const [characters, setCharacters] = useState<RickAndMortyCharacter[]>(response);
+    //loadCharacters()
+    const [characters, setCharacters] = useState<RickAndMortyCharacter[]>([]);
     const [searchText, setSearchText] = useState("");
+
+    useEffect(() => {
+        loadCharacters()
+    }, []);
 
     const filteredCharacters = characters
         .filter((character) => character.name.toLowerCase().includes(searchText.toLowerCase()));
 
     function addCharacter(character: RickAndMortyCharacter) {
         setCharacters([...characters, character]);
+    }
+
+    function loadCharacters()  {
+        axios.get("https://rickandmortyapi.com/api/character")
+            .then((response) => {
+                setCharacters(response.data.results)
+                return response.data
+            })
+            .catch((error) => {
+                console.error(error.message)
+            })
+            .finally(() => {
+                console.log("Finally")
+            })
     }
 
     return (
